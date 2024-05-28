87878ba21b136e1efc878ae28fd578272a070115
Baciul <mihaiadi09@gmail.com>
2024-05-28 10:52:40 +0300

    layout component

---
+++ docs/.vitepress/theme/Layout.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+      <Layout>
+        <slot />
+        <!-- Insert the rating system component at the bottom of the layout -->
+        <slot name="layout-bottom" />
+      </Layout>
+    </div>
+  </template>
+  
+  <script>
+  import Layout from 'vitepress/theme/default/Layout.vue'
+  
+  export default {
+    components: {
+      Layout
+    }
+  }
+  </script>
+  
