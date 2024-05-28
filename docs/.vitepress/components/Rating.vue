13b06ea6fe2db307d83e261011088b25f97e86c2
Baciul <mihaiadi09@gmail.com>
2024-05-28 11:19:26 +0300

    rating component

--- docs/.vitepress/components/Rating.vue
+++ docs/.vitepress/components/Rating.vue
@@ -1,15 +1,12 @@
-
 <template>
     <div class="rating-system">
-      <span class="header-font">Rate this page</span>
-      <div class="stars">
+      <span class="header-font" v-if="!rated">Rate this page</span>
+      <span style="font-family:'Conthrax'; padding-bottom: 30px;" v-else>Thank you for your feedback!</span>
+      <div class="stars" v-if="!rated">
         <span v-for="star in 5" :key="star" @click="rate(star)">
           <i :class="star <= rating ? 'fas fa-star' : 'far fa-star'"></i>
         </span>
       </div>
-    <transition name="fade">
-        <span style="font-family:'Conthrax'; padding-bottom: 30px;" v-if="rated">Thank you for your feedback!</span>
-    </transition>
     </div>
   </template>
   
@@ -27,7 +24,7 @@
         this.rating = star;
         this.rated = true;
         setTimeout(() => {
-        this.rated = false;
+          this.rated = false;
         }, 5000);
         // Here, you can send the rating to your server or any backend service
         console.log(`Page rated with ${star} stars.`);
@@ -41,32 +38,32 @@
     text-align: center;
     margin-top: 10px;
   }
-
+  
   .stars {
     font-size: 12px;
   }
-
+  
   .stars span {
     cursor: pointer;
     color: #ffca28;
   }
-
+  
   .stars span:hover {
     color: #ffa000;
   }
-
+  
   .fas.fa-star {
     color: #ffca28;
   }
-
+  
   .far.fa-star {
     color: #e0e0e0;
   }
-
+  
   .fade-enter-active, .fade-leave-active {
     transition: opacity 0.5s;
   }
-
+  
   .fade-enter, .fade-leave-to {
     opacity: 0;
   }
