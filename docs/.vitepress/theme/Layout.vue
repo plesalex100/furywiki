c84c796cef135222d5ee84b425bad4a182c9fca1
Baciul <mihaiadi09@gmail.com>
2024-06-05 10:25:43 +0300

    fade anim

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -1,20 +1,81 @@
 <template>
-    <div>
-      <Layout>
-        <slot />
-        <!-- Insert the rating system component at the bottom of the layout -->
-        <slot name="layout-bottom" />
-      </Layout>
-    </div>
-  </template>
+  <div>
+    <DefaultTheme.Layout>
+      <slot />
+      <!-- Insert the rating system component at the bottom of the layout -->
+      <slot name="layout-bottom" />
+    </DefaultTheme.Layout>
+  </div>
+</template>
   
-  <script>
-  import Layout from 'vitepress/theme/default/Layout.vue'
+<script setup lang="ts">
+  import { useData } from 'vitepress'
+  import DefaultTheme from 'vitepress/theme'
+  import { nextTick, provide } from 'vue'
   
-  export default {
-    components: {
-      Layout
-    }
+  const { isDark } = useData()
+
+  const enableTransitions = () =>
+    'startViewTransition' in document &&
+  window.matchMedia('(prefers-reduced-motion: no-preference)').matches
+  
+  provide('toggle-appearance', async ({ clientX: x, clientY: y }: MouseEvent) => {
+  if (!enableTransitions()) {
+    isDark.value = !isDark.value
+    return
   }
+
+  const clipPath = [
+    `circle(0px at ${x}px ${y}px)`,
+    `circle(${Math.hypot(
+      Math.max(x, innerWidth - x),
+      Math.max(y, innerHeight - y)
+    )}px at ${x}px ${y}px)`
+  ]
+
+  await document.startViewTransition(async () => {
+    isDark.value = !isDark.value
+    await nextTick()
+  }).ready
+
+  document.documentElement.animate(
+    { clipPath: isDark.value ? clipPath.reverse() : clipPath },
+    {
+      duration: 300,
+      easing: 'ease-in',
+      pseudoElement: `::view-transition-${isDark.value ? 'old' : 'new'}(root)`
+    }
+  )
+})
+  // export default {
+  //   components: {
+  //     Layout
+  //   }
+  // }
   </script>
   
+  <style>
+::view-transition-old(root),
+::view-transition-new(root) {
+  animation: none;
+  mix-blend-mode: normal;
+}
+
+::view-transition-old(root),
+.dark::view-transition-new(root) {
+  z-index: 1;
+}
+
+::view-transition-new(root),
+.dark::view-transition-old(root) {
+  z-index: 9999;
+}
+
+.VPSwitchAppearance {
+  width: 22px !important;
+}
+
+.VPSwitchAppearance .check {
+  transform: none !important;
+}
+</style>
