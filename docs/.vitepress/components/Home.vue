c33ade68c5e4287379761a75f04638953e886f00
Baciul <mihaiadi09@gmail.com>
2024-06-08 12:50:48 +0300

    clip component

--- docs/.vitepress/components/Home.vue
+++ docs/.vitepress/components/Home.vue
@@ -15,21 +15,21 @@ import SiteMap from './SiteMap.vue'
 
   <section id="highlights" class="vt-box-container">
     <div class="vt-box">
-      <h2 style="font-family:'Conthrax';color: orange;" href="/informatii/regulament.html">Roleplay Advanced</h2>
+      <h2 style="font-family:'Conthrax';color: orange;">Roleplay Advanced</h2>
       <p>
         Dorința noastră este ca nivelul de roleplay să fie cât mai complex și ridicat cu putință, 
         astfel încât să oferim o experiență cât mai plăcută și realistă jucătorilor noștri.
       </p>
     </div>
     <div class="vt-box">
-      <h2 style="font-family:'Conthrax';color: orange;" href="/informatii/updatesv.html">Update-uri constante</h2>
+      <h2 style="font-family:'Conthrax';color: orange;">Update-uri constante</h2>
       <p>
         Oferim constant Update-uri calitative pentru a îmbunătăți experiența jucătorilor noștri,
         astfel încât să existe mereu ceva nou și interesant de descoperit.
       </p>
     </div>
     <div class="vt-box">
-      <h2 style="font-family:'Conthrax';color: orange;" href="/informatii/about.html">Proiect pe termen lung</h2>
+      <h2 style="font-family:'Conthrax';color: orange;">Proiect pe termen lung</h2>
       <p>
         Proiectul Fury este unul care va fi pe termen lung, astfel puteți să vă bucurați de serverul nostru
         pentru o perioadă îndelungată, fără wipe-uri.
