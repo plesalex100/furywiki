175d2b9180e75185de7e48ab558fa50774bae0f4
Baciul <mihaiadi09@gmail.com>
2024-06-18 19:12:25 +0300

    anim

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -1,19 +1,39 @@
 <template>
   <div>
     <Layout>
-      <slot />
-      <!-- Insert the rating system component at the bottom of the layout -->
+      <transition name="fade" mode="out-in">
+        <!-- Replace router-view with component :is -->
+        <component :is="currentPage.component"></component>
+      </transition>
+      <!-- Preserve existing slot for layout-bottom -->
       <slot name="layout-bottom" />
     </Layout>
   </div>
 </template>
 
 <script>
-import Layout from 'vitepress/theme/default/Layout.vue'
+import { defineComponent, ref } from 'vue';
+import Layout from 'vitepress/theme/default/Layout.vue';  // Adjust path as needed
+import config from './config.ts';
 
-export default {
+export default defineComponent({
+  name: 'CustomLayout',
   components: {
     Layout
+  },
+  setup() {
+    const currentPage = ref(null);
+
+    // Load initial page
+    currentPage.value = config.pages[0];
+
+    return {
+      currentPage
+    };
   }
-}
+});
 </script>
+
+<style lang="css">
+@import './style.css';
+</style>
