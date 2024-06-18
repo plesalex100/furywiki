9fb45d549a8b9027c767d5a7b06cc5185d7eb581
Baciul <mihaiadi09@gmail.com>
2024-06-18 20:09:23 +0300

    anim

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,7 +1,7 @@
 import { h } from 'vue'
 import Theme from 'vitepress/theme'
+import PageTransition from '../components/PageTransition.vue'
 import RatingSystem from '../components/Rating.vue'
-import PageTransition from '../components/PageTransition.vue' // Import PageTransition component
 import '@fortawesome/fontawesome-free/css/all.css'
 import './style.css'
 
@@ -9,17 +9,14 @@ export default {
   ...Theme,
   Layout: () => {
     return h(Theme.Layout, null, {
-      // Add PageTransition to wrap the default layout content
       default: () => h(PageTransition, null, {
         default: () => h(Theme.Layout)
       }),
-      // Add RatingSystem to layout-bottom slot
       'layout-bottom': () => h(RatingSystem)
     })
   },
   enhanceApp({ app }) {
-    // Register components globally
-    app.component('RatingSystem', RatingSystem)
     app.component('PageTransition', PageTransition)
+    app.component('RatingSystem', RatingSystem)
   }
 }
