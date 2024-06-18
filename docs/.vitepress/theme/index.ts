10c9194de6027b26a615aa45a41bfca00a82447d
Baciul <mihaiadi09@gmail.com>
2024-06-18 17:52:50 +0300

    page switch anim

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,5 +1,6 @@
 // https://vitepress.dev/guide/custom-theme
 import { h } from 'vue'
+import { RouterView } from 'vue-router'
 import Theme from 'vitepress/theme'
 import RatingSystem from '../components/Rating.vue'
 import '@fortawesome/fontawesome-free/css/all.css'
@@ -10,10 +11,29 @@ export default {
   Layout: () => {
     return h(Theme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
-      'layout-bottom': () => h(RatingSystem)
+      'layout-bottom': () => h(RatingSystem),
+      default: () => h('transition', {
+        name: 'slide'
+      }, {
+        default: () => h(RouterView)
+      })
     })
   },
-  enhanceApp({ app }) {
+  enhanceApp({ app, router }) {
     app.component('RatingSystem', RatingSystem)
+
+    // Global navigation guards for route changes
+    router.beforeEach((to, from, next) => {
+      const content = document.querySelector('.content')
+      if (content) {
+        content.classList.add('slide-leave-active')
+        setTimeout(() => {
+          content.classList.remove('slide-leave-active')
+          next()
+        }, 300) // Match the duration of your transition
+      } else {
+        next()
+      }
+    })
   }
 }
