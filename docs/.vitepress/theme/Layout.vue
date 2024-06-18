a766d09818710719e57352c18a66cd736d3da236
Baciul <mihaiadi09@gmail.com>
2024-06-18 19:40:38 +0300

    anim

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -1,19 +1,24 @@
 <template>
   <div>
     <Layout>
-      <slot />
-      <!-- Insert the rating system component at the bottom of the layout -->
+      <PageTransition>
+        <!-- This slot will contain the content of your pages -->
+        <slot />
+      </PageTransition>
+      <!-- Optionally, you can insert other components or content here -->
       <slot name="layout-bottom" />
     </Layout>
   </div>
 </template>
 
 <script>
-import Layout from 'vitepress/theme/default/Layout.vue'
+import Layout from 'vitepress/theme/default/Layout.vue';
+import PageTransition from '../components/PageTransition.vue';
 
 export default {
   components: {
-    Layout
+    Layout,
+    PageTransition
   }
 }
 </script>
