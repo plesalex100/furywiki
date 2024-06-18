80239c793decc0db286859326b3fefc3db85b7dd
Baciul <mihaiadi09@gmail.com>
2024-06-18 18:09:37 +0300

    layout revert

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,22 +1,14 @@
-// https://vitepress.dev/guide/custom-theme
-import { h, defineComponent } from 'vue'
+import { h } from 'vue'
 import Theme from 'vitepress/theme'
 import RatingSystem from '../components/Rating.vue'
-import '@fortawesome/fontawesome-free/css/all.css'
 import './style.css'
-
-const CustomLayout = defineComponent({
-  setup() {
-    return () => h(Theme.Layout, null, {
-      'layout-bottom': () => h(RatingSystem),
-      default: () => h('transition', { name: 'slide' }, () => h('RouterView'))
-    })
-  }
-})
 
 export default {
   ...Theme,
-  Layout: CustomLayout,
+  Layout: () => h(Theme.Layout, null, {
+    'layout-bottom': () => h(RatingSystem),
+    default: () => h('transition', { name: 'slide' }, () => h('RouterView'))
+  }),
   enhanceApp({ app }) {
     app.component('RatingSystem', RatingSystem)
   }
