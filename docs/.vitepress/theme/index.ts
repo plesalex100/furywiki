1a7cf12ec636c6b8a17931b98ea6da7a4c229f44
Baciul <mihaiadi09@gmail.com>
2024-06-18 17:54:44 +0300

    page switch anim

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,39 +1,24 @@
 // https://vitepress.dev/guide/custom-theme
-import { h } from 'vue'
-// import { RouterView } from 'vue-router'
+import { h, defineComponent } from 'vue'
 import Theme from 'vitepress/theme'
 import RatingSystem from '../components/Rating.vue'
 import '@fortawesome/fontawesome-free/css/all.css'
 import './style.css'
 
+const CustomLayout = defineComponent({
+  setup() {
+    return () => h('div', { class: 'custom-layout' }, [
+      h('transition', { name: 'slide' }, [
+        h(Theme.Layout)
+      ])
+    ])
+  }
+})
+
 export default {
   ...Theme,
-  Layout: () => {
-    return h(Theme.Layout, null, {
-      // https://vitepress.dev/guide/extending-default-theme#layout-slots
-      'layout-bottom': () => h(RatingSystem),
-      default: () => h('transition', {
-        name: 'slide'
-      }, {
-        default: () => h(RouterView)
-      })
-    })
-  },
-  enhanceApp({ app, router }) {
+  Layout: CustomLayout,
+  enhanceApp({ app }) {
     app.component('RatingSystem', RatingSystem)
-
-    // Global navigation guards for route changes
-    router.beforeEach((to, from, next) => {
-      const content = document.querySelector('.content')
-      if (content) {
-        content.classList.add('slide-leave-active')
-        setTimeout(() => {
-          content.classList.remove('slide-leave-active')
-          next()
-        }, 300) // Match the duration of your transition
-      } else {
-        next()
-      }
-    })
   }
 }
