14c3fb1aac7ee3103d1bc91a62e3601d755639c0
Baciul <mihaiadi09@gmail.com>
2024-06-18 19:38:28 +0300

    anim component init

---
+++ docs/.vitepress/components/PageTransition.vue
@@ -0,0 +1,15 @@
+<template>
+    <transition name="fade">
+      <slot></slot>
+    </transition>
+  </template>
+  
+  <style scoped>
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity 0.5s;
+  }
+  .fade-enter, .fade-leave-to {
+    opacity: 0;
+  }
+  </style>
+  
