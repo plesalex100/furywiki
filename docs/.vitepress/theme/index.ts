0287f6677bfe5104cbd8d431aa57441ce56e93f7
Baciul <mihaiadi09@gmail.com>
2024-06-18 18:26:13 +0300

    page switch anim

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,21 +1,18 @@
-import { h, defineComponent } from 'vue'
+// https://vitepress.dev/guide/custom-theme
+import { h } from 'vue'
 import Theme from 'vitepress/theme'
 import RatingSystem from '../components/Rating.vue'
 import '@fortawesome/fontawesome-free/css/all.css'
 import './style.css'
 
-const CustomLayout = defineComponent({
-  setup() {
-    return () => h(Theme.Layout, null, {
-      'layout-bottom': () => h(RatingSystem),
-      default: () => h('transition', { name: 'slide' }, () => h('RouterView'))
-    })
-  }
-})
-
 export default {
   ...Theme,
-  Layout: CustomLayout,
+  Layout: () => {
+    return h(Theme.Layout, null, {
+      // https://vitepress.dev/guide/extending-default-theme#layout-slots
+      'layout-bottom': () => h(RatingSystem)
+    })
+  },
   enhanceApp({ app }) {
     app.component('RatingSystem', RatingSystem)
   }
