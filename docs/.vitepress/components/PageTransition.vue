f5579406cfe2a4d69f597e771a6131e9f9a08d36
Baciul <mihaiadi09@gmail.com>
2024-06-18 20:09:45 +0300

    anim

--- docs/.vitepress/components/PageTransition.vue
+++ docs/.vitepress/components/PageTransition.vue
@@ -4,6 +4,12 @@
     </transition>
   </template>
   
+  <script>
+  export default {
+    name: 'PageTransition'
+  }
+  </script>
+  
   <style scoped>
   .fade-enter-active, .fade-leave-active {
     transition: opacity 0.5s;
