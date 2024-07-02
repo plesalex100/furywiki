08f556d6c13441dbad95a703acd7f778b9ca2793
Baciu09 <mihaiadi09gmail.com>
2024-07-03 00:37:50 +0300

    remove

--- docs/.vitepress/components/Rating.vue
+++ docs/.vitepress/components/Rating.vue
@@ -32,9 +32,8 @@
           this.rated = true;
           setTimeout(() => {
             this.rated = false;
-          }, 3000); // Delay before fading out the "Thank you" message
-        }, 500); // Delay before fading out the rating and stars
-        // Here, you can send the rating to your server or any backend service
+          }, 3000);
+        }, 500);
         console.log(`Page rated with ${star} stars.`);
       }
     }
